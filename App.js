df4bb44777c08da42b30342a129e833de88eab14
Pranavi-06 <pranavisakamuri@gmail.com>
2024-07-05 10:29:13 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import Counter from './state';
+
+const App = () => {
+  return (
+    <div>
+      <Counter />
+    </div>
+  );
+};
+
+export default App;
